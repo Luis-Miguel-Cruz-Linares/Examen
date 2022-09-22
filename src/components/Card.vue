a76d710cd73f36421586bb499bc59ce0acfc83c6
Luis-Miguel-Cruz-Linares <a3521110400@alumno.uttehuacan.edu.mx>
2022-09-22 11:33:58 -0500

    examen

---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <Grid>
+      <img :src="url" :alt="nombre">
+      <h2>{{ nombre }}</h2> 
+      <h4>{{ descripcion }}</h4>
+      <h3>{{ costo }}</h3>
+      <a :href="url" target="_blank">Ordenar</a>
+  </Grid>
+</template>
+
+<script>
+  import Grid from "./Grid.vue";
+  export default{
+  props: {
+      url: {
+          String,
+      },
+      nombre: {
+          String,
+      },
+      descripcion: {
+          String,
+      },
+      costo: {
+          String,
+      },
+  },
+  components: { Grid },
+};
+</script>
+
+<style scopet>
+
+  img{
+      width: 8  rem;
+      height: 8rem;
+      padding: 0.5rem;
+      background:aqua;
+      border-radius: 30px;
+  }
+  a{
+      text-decoration: none;
+      color: rgb(41, 206, 123);
+      background: black;
+      padding: 0.3rem;
+      border-radius: 5px;
+      border: 1px solid #ffffff;
+      margin-bottom: 60px;
+  }
+  
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <slot>  </slot>
+  </div>
+</template>
+
+<style scopet>
+     div {
+      text-align:center ;
+      border: 1px solid #ffffff;
+      padding: 1%;
+      border-radius: 9px;
+      padding: 10px;
+      margin-left: 50px;
+      margin-bottom: 70px;
+     color:black;
+       }
+
+    
+</style>  
